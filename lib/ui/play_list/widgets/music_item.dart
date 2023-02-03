9ed18b80a66053dc9994d07d9bcbc08da01d22e3
Samandar <samandarahadjonov@gmail.com>
2023-02-03 11:09:03 +0500

    ui done

--- lib/ui/play_list/widgets/music_item.dart
+++ lib/ui/play_list/widgets/music_item.dart
@@ -1,30 +1,37 @@
 import 'package:flutter/material.dart';
-
+import 'package:music_payer/ui/single_music/single_music_page.dart';
+import 'package:zoom_tap_animation/zoom_tap_animation.dart';
+import 'package:page_transition/page_transition.dart';
 class MusicItem extends StatelessWidget {
   const MusicItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin:const  EdgeInsets.only(bottom: 12),
-      height: 60,
-      width: 400,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Image.network("https://avatars.mds.yandex.net/i?id=89e8bb990974a1d4a42be9e5a899714b8bcc3f73-5210406-images-thumbs&n=13"),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const  [
-              Text("Happier Than Ever",style: TextStyle(fontSize: 18,color: Colors.white),),
-              Text("Billie Eilish - Happier Than Ever",style: TextStyle(color: Colors.white),)
+    return InkWell(
+      onTap: () {
+        Navigator.push(context, PageTransition(type: PageTransitionType.bottomToTop, child:   SingleMusicPage(),duration: const Duration(milliseconds: 200)));
+      },
+      child: Container(
+        margin:const  EdgeInsets.only(bottom: 12),
+        height: 60,
+        width: 400,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Image.network("https://avatars.mds.yandex.net/i?id=89e8bb990974a1d4a42be9e5a899714b8bcc3f73-5210406-images-thumbs&n=13"),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const  [
+                Text("Happier Than Ever",style: TextStyle(fontSize: 18,color: Colors.white),),
+                Text("Billie Eilish - Happier Than Ever",style: TextStyle(color: Colors.white),)
 
-            ],
-          ),
-          IconButton(onPressed: (){}, icon: Icon(Icons.favorite,color: Colors.green,)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.menu,color: Colors.white,)),
-        ],
+              ],
+            ),
+            IconButton(onPressed: (){}, icon: Icon(Icons.favorite,color: Colors.green,)),
+            IconButton(onPressed: (){}, icon: Icon(Icons.menu,color: Colors.white,)),
+          ],
+        ),
       ),
     );
   }
--- lib/ui/single_music/single_music_page.dart
+++ lib/ui/single_music/single_music_page.dart
@@ -1,10 +1,75 @@
 import 'package:flutter/material.dart';
 
-class SingleMusicPage extends StatelessWidget {
-  const SingleMusicPage({Key? key}) : super(key: key);
+class SingleMusicPage extends StatefulWidget {
+   SingleMusicPage({Key? key}) : super(key: key);
+
+  @override
+  State<SingleMusicPage> createState() => _SingleMusicPageState();
+}
+
+class _SingleMusicPageState extends State<SingleMusicPage> {
+  int currentValue=20;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text("Liked Music",style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        leading: IconButton(onPressed: () {
+          Navigator.pop(context);
+
+        },icon: const Icon(Icons.arrow_downward_sharp,color: Colors.white,)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 350,
+              width: 400,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image:  NetworkImage("https://avatars.mds.yandex.net/i?id=89e8bb990974a1d4a42be9e5a899714b8bcc3f73-5210406-images-thumbs&n=13"),
+                  fit: BoxFit.fitHeight
+
+                )
+              ),
+            ),
+            const SizedBox(height: 42,),
+            const Text("Happier Than Others",style: TextStyle(color: Colors.white,fontWeight: FontWeight.w700,fontSize: 20),),
+            const SizedBox(height: 8,),
+            const Text("Billie Eilish",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.w500,fontSize: 20),),
+            const SizedBox(height: 32,),
+            Slider(
+              inactiveColor: Colors.grey,
+              activeColor: Colors.white,
+              max: 100,
+              value: currentValue.toDouble(),
+              onChanged: (value) {
+              setState(() {
+                currentValue = value.toInt();
+              });
+            },),
+            const SizedBox(height: 32,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(onPressed: () {}, icon: Icon(Icons.shuffle,color: Colors.white,)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.skip_previous,size: 40,color: Colors.white,)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.play_circle,size: 40,color: Colors.white,)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.skip_next,size:40,color: Colors.white,)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.repeat,color: Colors.white,)),
+              ],
+            )
+
+
+          ],
+        ),
+      ),
+    );
   }
 }
